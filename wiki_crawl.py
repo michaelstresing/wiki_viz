fd7d483f2c741569b7f12988beee4fed107bfef9
michaelstresing <mstresing@gmail.com>
2019-10-22 20:17:57 +0800

    progress from afternoon

--- wiki_crawl.py
+++ wiki_crawl.py
@@ -1,17 +1,49 @@
 from bs4 import BeautifulSoup as bs4
+from urllib.parse import urlparse
 import requests
-from xml.etree import ElementTree
+from queue import Queue
+from graphviz import Graph
 
 
 class LinkUtil:
 
     @staticmethod
     def is_anchor(link):
-        return len(link) > 0 and link[0] == '#'
+        if len(link) > 0 and link[0] == '#':
+            return True
+        else:
+            return False
 
     @staticmethod
     def is_wiki_link(link):
         return len(link) > 0 and link[0:6] == '/wiki/'
+
+    @staticmethod
+    def is_wiki_special_type(link):
+        return len(link) > 0 and (link[6:15] == 'Category:'
+                                  or link[6:14] == 'Special:'
+                                  or link[6:11] == 'Talk:'
+                                  or link[6:11] == 'Help:'
+                                  or link[6:11] == 'File:'
+                                  )
+
+    @staticmethod
+    def is_image(link):
+        return len(link) > 0 and (link[:-4] == '.png'
+                                  or link[:-4] == '.jpg'
+                                  or link[:-5] == '.jpeg'
+                                  )
+
+    @staticmethod
+    def is_active_link(link):
+        r = requests.get(link)
+        return 199 < r.status_code < 300
+
+    @staticmethod
+    def is_html_link(link):
+        r = requests.get(link)
+        h = r.headers['Content-Type']
+        return h[0:9] == 'text/html'
 
 
 class Crawler:
@@ -23,6 +55,7 @@ class Crawler:
 
         self.url = url
         self.depth = depth
+        self._queue = Queue()
 
     __create_key = object()
 
@@ -34,40 +67,101 @@ class Crawler:
 
         response = requests.get(self.url)
         body = response.content
+        soup = bs4(body, "html.parser")
 
         if 199 < response.status_code < 300:
-            return body
+            return soup
         else:
             assert "Invalid Request"
 
-    def process_link(self, link):
+    @staticmethod
+    def pull_processed_links(url):
+
+        response = requests.get(url)
+        body = response.content
+        soup = bs4(body, "html.parser")
+
+        if 199 < response.status_code < 300:
+            pass
+        else:
+            assert "Invalid Request"
+
+        page_links = set()
+        all_links = [a.get('href') for a in soup.find_all("a", href=True)]
+
+        for link in all_links:
+            link = Crawler.process_link_in(link)
+            if link:
+                page_links.add(link)
+
+        return page_links
+
+    @staticmethod
+    def process_link_in(link):
+
         if LinkUtil.is_anchor(link):
             return None
-        if not LinkUtil.is_wiki_link(link):
+        elif not LinkUtil.is_wiki_link(link):
+            return None
+        elif LinkUtil.is_wiki_special_type(link):
+            return None
+        elif LinkUtil.is_image(link):
             return None
 
         # more link processing and filtering
 
         return link
 
-    def pull_raw_links(self, html):
+    @staticmethod
+    def process_link_out(link):
 
-        soup = bs4(html, "html.parser")
-        page_links = [a.get('href') for a in soup.find_all("a", href=True)]
+        if not LinkUtil.is_active_link(link):
+            return None
+        elif not LinkUtil.is_html_link(link):
+            return None
 
-        return page_links
+        return link
 
-    # def extract_broken_links(self, links):
-    #
-    #     extracted_links = list()
-    #
-    #     for link in links:
-    #         req = requests.get(link)
-    #
-    #         if 199 < req.status_code < 299:
-    #             extracted_links.append(link)
-    #
-    #     return extracted_links
+    def write_relationship(self, parent, child):
+
+        file = f"{self.url.replace('/', '_').replace(':', '_')}_relations.txt"
+
+        with open(f"page_relations/{file}", 'a+') as fout:
+            fout.write(f"   {parent} -> {child};\n")
+
+        g = Graph('G', filename=f"{self.url.replace('/', '_').replace(':', '_')}_newtork.gv'", engine='sfdp')
+
+        g.edge({parent}, {child})
+
+    def construct_url(self, link):
+        """
+        Takes a link's path and constucts the full URL based on the original website scheme and domain.
+        """
+        url = self.url
+        parsedurl = urlparse(url)
+
+        return parsedurl[0] + "://" + parsedurl[1] + link
+
+    def run_crawler(self):
+
+        q = self._queue
+        q.put((self.url, 0))
+
+        while q.qsize() > 0:
+
+            item = q.get()
+            url = item[0]
+            layer = item[1]
+
+            links = Crawler.pull_processed_links(url)
+
+            for link in links:
+
+                fulllink = Crawler.construct_url(self=self, link=link)
+                Crawler.write_relationship(self=self, parent=url, child=fulllink)
+
+                if layer < self.depth:
+                    q.put((fulllink, layer + 1))
 
 
 class BuildCrawler:
@@ -102,6 +196,5 @@ class BuildCrawler:
 
 testc = Crawler.new()
 testc = testc.to_crawler()
-html = testc.retrieve_html()
-testc.pull_links(html)
-print(html)
+
+testc.run_crawler()
